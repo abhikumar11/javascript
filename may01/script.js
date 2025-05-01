dd62ae2553471ee96c5d72ce4ab3d24e6609847a
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-05-01 13:30:17 +0530

    initial commit

---
+++ may01/script.js
@@ -0,0 +1,22 @@
+var name="Jhon";
+var age=20;
+console.log(name);
+console.log(age);
+var x;
+console.log(x,typeof(x))
+
+var firstName="harry";
+console.log(firstName,typeof(firstName));
+
+var value=null;
+console.log(value,typeof(value));
+//datatype of null is object
+
+var isEligible=true;
+console.log(isEligible,typeof(isEligible));
+
+var num=535659n;
+console.log(num,typeof(num));
+
+var id=Symbol(id);
+console.log(id,typeof(id));
