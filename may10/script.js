fb64151adb040f6140294c72473ff9715981652a
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-05-10 13:31:55 +0530

    initial commit

--- may10/script.js
+++ may10/script.js
@@ -1,5 +1,5 @@
 //array and array methods
-const numbers=[1,2,3,4,5];
+const numbers=[1,2,3,4,5,6];
 console.log(numbers[0]);
 let a=numbers[1];
 console.log(a);
@@ -16,4 +16,45 @@ console.log(mixed[4].age);
 
 let x=numbers.length;
 x=numbers.indexOf(5);
+x=numbers.includes(7);
 console.log(x);
+numbers.push(7);
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+numbers.unshift(10);//remove first element
+console.log(numbers);
+
+numbers.shift();//remove first element
+console.log(numbers);
+
+numbers.reverse();//reverse
+console.log(numbers);
+
+x=numbers.toString();
+x=numbers.join();//converts the array into string
+console.log(x);
+//slice returs a new array with selected elements it does not change the original array
+x=numbers.slice(0,4);
+console.log(x);
+//splice(starting index,number of elements to remove from starting index)
+x=numbers.splice(1,2);
+console.log(x);
+console.log(numbers);
+
+x=numbers.splice(1,3,100,200,300);
+console.log(x);
+console.log(numbers);
+
+const arr=[10,20,30,40,50,60,70];
+x=arr.slice(1,4).reverse().toString().charAt(0);
+console.log(x);
+
+const fruits=["apple","kiwi","orange"];
+const veg=["potato","tomato","peas"];
+let full=[fruits,veg];
+x=full[1][0];
+console.log(full);
+console.log(x);
+full=fruits.concat(veg);
+console.log(full);
