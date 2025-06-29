01a091bfcdb0e8fbf89fd14bdf7b738d4e6d20d9
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-06-29 09:08:40 +0530

    initial commit

--- mini projects/dice/script.js
+++ mini projects/dice/script.js
@@ -1,6 +1,6 @@
   const roll=()=> {
-  const dice = document.getElementById("dice");
-  const display = document.getElementById("display");
+  const dice = document.querySelector("#dice");
+  const display = document.querySelector("#display");
   const rollValue = Math.floor(Math.random() * 6) + 1;
   dice.src = `images/face${rollValue}.png`;
   display.textContent = `You rolled a ${rollValue}`;
---
+++ mini projects/digitalclock/script.js
@@ -0,0 +1,14 @@
+function updateClock() {
+     const now = new Date();
+     let hours = now.getHours().toString().padStart(2, "0");
+     let minutes = now.getMinutes().toString().padStart(2, "0");
+     let seconds = now.getSeconds().toString().padStart(2, "0");
+     const ampm = hours >= 12 ? "PM" : "AM";
+     hours = hours % 12 || 12;
+     const timeString = `${hours
+          .toString()
+          .padStart(2, "0")}:${minutes}:${seconds} ${ampm}`;
+     document.getElementById("clock").textContent = timeString;
+}
+updateClock();
+setInterval(updateClock, 1000);
