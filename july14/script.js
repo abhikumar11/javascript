b81d43069ee7235889a5be8135f5cfebe16cf509
abhikumar11 <abhishekkumar12verma@gmail.com>
2025-07-14 23:35:18 +0530

    initial commit

--- july14/script.js
+++ july14/script.js
@@ -1,12 +1,14 @@
 let counter=0;
 const increase=()=>{
     let display=document.querySelector("#cnt");
-    ++counter;
+     ++counter;
+    counter%2!==0?display.style.color="red":display.style.color="black";
     display.innerHTML=counter;
 }
 const decrease=()=>{
     let display=document.querySelector("#cnt");
     counter==0?counter=0:--counter;
+    counter%2!=0?display.style.color="red":display.style.color="black";
     display.innerHTML=counter;
 }
 
